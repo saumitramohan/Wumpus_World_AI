4f1c827e2e7b6df1ed6637bc30de2c63fe5c04e2
Saumitra Shukla <saumitra90@gmail.com>
2018-03-15 01:06:45 -0700

    Draft AI

--- Wumpus_World_Java_Shell/src/MyAI.java
+++ Wumpus_World_Java_Shell/src/MyAI.java
@@ -1,10 +1,13 @@
 import java.util.HashMap;
 import java.util.Stack;
+import java.util.Random;
+import java.util.*;
 
 class DirectionsEnum{
 	
 	public String value;
 	public int id;
+	static int i = 0;
 	
 	public DirectionsEnum(int id, String value) {
 		this.id = id;
@@ -134,124 +137,117 @@ public class MyAI extends Agent {
 		agentYLimit = 10;
 	}
 
-	public Action getAction(boolean stench, boolean breeze, boolean glitter, boolean bump, boolean scream) {
-		if (glitter) {
-			isGoldGrabbed = true;
-			return Action.GRAB;
-		}
+	public void reduceAgentBoundaries(){
+		Cell cell = agentPath.getLastVisitedCell();
 
-		if (isGoldGrabbed) {
-			if (agentPath.isEmpty()) {
-				return Action.CLIMB;
-			}
-
-			Cell cell = agentPath.peekLastVisitedCell();
-			Action action = nextAction(cell, agent);
-
-			if (action == Action.FORWARD) {
-				agent.x = cell.x;
-				agent.y = cell.y;
-				agentPath.getLastVisitedCell();
-			} else {
-				editAgentDirection(agent, action);
-			}
-
-			return action;
-		}
-
-		if (bump) {
-			Cell cell = agentPath.getLastVisitedCell();
-
-			if (agent.currentDirection == DirectionsEnum.EAST) {
-				agentXLimit = agent.x;
-			}
 			if (agent.currentDirection == DirectionsEnum.NORTH) {
 				agentYLimit = agent.y;
 			}
 
+			else if (agent.currentDirection == DirectionsEnum.EAST) {
+				agentXLimit = agent.x;
+			}
+
 			agent.x = cell.x;
 			agent.y = cell.y;
-		}
+	}
 
-		boolean safeCondition = (!stench && !breeze);
-		boolean unsafeCondition = (stench || breeze);
-		if (safeCondition || unsafeCondition) {
-			if (safeCondition) {
-				markNeighborinfCellsSafeForTraversal(agent.x, agent.y);
-			}
-
-			this.grid.get(agent.x, agent.y).setSafeAndVisited();
-
-			Cell nextUnvisitedSafeCell = getNextUnvisitedSafeCell(agent.x, agent.y);
-
-			if (agentPath.isEmpty() && (nextUnvisitedSafeCell == null)) {
-				return Action.CLIMB;
-			}
-
-			if (nextUnvisitedSafeCell == null) {
-				Cell destinationCell = agentPath.peekLastVisitedCell();
-				Action currAction = nextAction(destinationCell, agent);
-
-				if (currAction != Action.FORWARD) {
-					editAgentDirection(agent, currAction);
-				}
-				else {
-					agent.x = destinationCell.x;
-					agent.y = destinationCell.y;
-					agentPath.getLastVisitedCell();
-				}
-				return currAction;
-			} else {
-				Action currAction = nextAction(nextUnvisitedSafeCell, agent);
-
-				if (currAction != Action.FORWARD) {
-					editAgentDirection(agent, currAction);
-				}
-				else {
-					agentPath.addCell(grid.get(agent.x, agent.y));
-					agent.x = nextUnvisitedSafeCell.x;
-					agent.y = nextUnvisitedSafeCell.y;
-				}
-				return currAction;
-			}
-		}
-
+	public Action backtrack(){
+		// reached exit point
 		if (agentPath.isEmpty()) {
 			return Action.CLIMB;
 		}
-		return Action.CLIMB;
+
+		Cell cell = agentPath.peekLastVisitedCell();
+		Action action = nextAction(cell, agent);
+
+		if (action == Action.FORWARD) {
+			agent.x = cell.x;
+			agent.y = cell.y;
+			agentPath.getLastVisitedCell();
+		} else {
+			changeAgentDirection(agent, action);
+		}
+
+		return action;
 	}
 
-	private Cell getNextUnvisitedSafeCell(int x, int y) {
-		
-		if (y + 1 < agentYLimit && y + 1 >= 0) {
-			if ((grid.get(x, y + 1).getIsSafe()) && (!grid.get(x, y + 1).getIsVisited())) {
-				return grid.get(x, y + 1);
-			}
-		}
-		
-		if (x + 1 < agentXLimit && x + 1 >= 0) {
-			if ((grid.get(x + 1, y).getIsSafe()) && (!grid.get(x + 1, y).getIsVisited())) {
-				return grid.get(x + 1, y);
-			}
-		}
+	public Action getAction(boolean stench, boolean breeze, boolean glitter, boolean bump, boolean scream) {
+		boolean safeCondition = (!stench && !breeze);
+		boolean unsafeCondition = (stench || breeze);
 
-		if (y - 1 < agentYLimit && y - 1 >= 0) {
-			if ((grid.get(x, y - 1).getIsSafe()) && (!grid.get(x, y - 1).getIsVisited())) {
-				return grid.get(x, y - 1);
+		if (glitter) {
+			isGoldGrabbed = true;
+			return Action.GRAB;
+		} else {
+			if (bump) {
+				reduceAgentBoundaries();
 			}
+
+			if (isGoldGrabbed) {
+				return backtrack();
+			}
+
+			if (safeCondition || unsafeCondition) {
+				if (safeCondition) {
+					// mark neighnors as safe
+					markNeighborinfCellsSafeForTraversal(agent.x, agent.y);
+				}
+
+				this.grid.get(agent.x, agent.y).setSafeAndVisited();
+
+				Cell nextUnvisitedSafeCell = getNextUnvisitedSafeCell(agent.x, agent.y);
+
+				if (nextUnvisitedSafeCell == null) {
+					if (agentPath.isEmpty()) {
+						return Action.CLIMB;
+					}
+					Cell destinationCell = agentPath.peekLastVisitedCell();
+					Action currAction = nextAction(destinationCell, agent);
+
+					if (currAction == Action.FORWARD) {
+						agent.x = destinationCell.x;
+						agent.y = destinationCell.y;
+						agentPath.getLastVisitedCell();
+					}
+					else {
+						changeAgentDirection(agent, currAction);
+					}
+					return currAction;
+				} else {
+					Action currAction = nextAction(nextUnvisitedSafeCell, agent);
+
+					if (currAction == Action.FORWARD) {
+						agentPath.addCell(grid.get(agent.x, agent.y));
+						agent.x = nextUnvisitedSafeCell.x;
+						agent.y = nextUnvisitedSafeCell.y;
+					}
+					else {
+						changeAgentDirection(agent, currAction);
+					}
+					return currAction;
+				}
+			}
+			return Action.CLIMB;
 		}
-		
+	}
+
+	private void markNeighborinfCellsSafeForTraversal(int x, int y) {	
 		if (x - 1 < agentXLimit && x - 1 >= 0) {
-			if ((grid.get(x - 1, y).getIsSafe()) && (!grid.get(x - 1, y).getIsVisited())) {
-				return grid.get(x - 1, y);
-			}
+			grid.get(x - 1, y).setIsSafe(Boolean.TRUE);
 		}
-
-		return null;
+		if (y + 1 < agentYLimit && y + 1 >= 0) {
+			grid.get(x, y + 1).setIsSafe(Boolean.TRUE);
+		}
+		if (y - 1 < agentYLimit && y - 1 >= 0) {
+			grid.get(x, y - 1).setIsSafe(Boolean.TRUE);
+		}
+		if (x + 1 < agentXLimit && x + 1 >= 0) {
+			grid.get(x + 1, y).setIsSafe(Boolean.TRUE);
+		}
 	}
 
-	private void editAgentDirection(MyAgentImpl myAgent, Action currAction) {
+	private void changeAgentDirection(MyAgentImpl myAgent, Action currAction) {
 		int val = myAgent.currentDirection.id;
 		if (currAction == Action.TURN_LEFT) {
 			val++;
@@ -321,18 +317,35 @@ public class MyAI extends Agent {
 		return Action.TURN_RIGHT;
 	}
 
-	private void markNeighborinfCellsSafeForTraversal(int x, int y) {	
-		if (x - 1 < agentXLimit && x - 1 >= 0) {
-			grid.get(x - 1, y).setIsSafe(Boolean.TRUE);
-		}
-		if (y + 1 < agentYLimit && y + 1 >= 0) {
-			grid.get(x, y + 1).setIsSafe(Boolean.TRUE);
-		}
-		if (y - 1 < agentYLimit && y - 1 >= 0) {
-			grid.get(x, y - 1).setIsSafe(Boolean.TRUE);
-		}
-		if (x + 1 < agentXLimit && x + 1 >= 0) {
-			grid.get(x + 1, y).setIsSafe(Boolean.TRUE);
+	private Cell getNextUnvisitedSafeCell(int x, int y) {
+
+	List<Cell> nextVertices = new ArrayList<Cell>();
+	if (y + 1 >= 0 && y + 1 < agentYLimit) {
+		if ((grid.get(x, y + 1).getIsSafe()) && (!grid.get(x, y + 1).getIsVisited())) {
+			nextVertices.add(grid.get(x, y + 1));
 		}
 	}
+	
+	if (x + 1 >= 0 && x + 1 < agentXLimit) {
+		if ((grid.get(x + 1, y).getIsSafe()) && (!grid.get(x + 1, y).getIsVisited())) {
+			nextVertices.add(grid.get(x + 1, y));
+		}
+	}
+
+	if (y - 1 >= 0 && y - 1 < agentYLimit) {
+		if ((grid.get(x, y - 1).getIsSafe()) && (!grid.get(x, y - 1).getIsVisited())) {
+			nextVertices.add(grid.get(x, y - 1));
+		}
+	}
+	
+	if (x - 1 >= 0 && x - 1 < agentXLimit) {
+		if ((grid.get(x - 1, y).getIsSafe()) && (!grid.get(x - 1, y).getIsVisited())) {
+			nextVertices.add(grid.get(x - 1, y));
+		}
+	}
+	if(nextVertices.size() == 0)
+		return null;
+	return nextVertices.get(new Random().nextInt(nextVertices.size()));
+	} 
+
 }
